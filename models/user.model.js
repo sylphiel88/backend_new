e39e19c147d7a65dd9fd3ca2180e5ffd81f0f173
sylphiel88 <jihmels@gmail.com>
2021-10-07 16:00:22 +0200

    login

--- models/user.model.js
+++ models/user.model.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose')
 
-var validateEmail = function(email) {
+SALT_WORK_FACTOR = 10
+const bcrypt = require("bcrypt")
+
+var validateEmail = function (email) {
     var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     return re.test(email)
 };
 
 
 const UserSchema = mongoose.Schema({
-    userName: {
+    username: {
         type: String,
         minlength: 3,
         required: true
@@ -28,12 +31,30 @@ const UserSchema = mongoose.Schema({
     },
     isActivated: {
         type: Boolean,
-        required: true,
+        default: false,
     },
     isDeleted: {
         type: Boolean,
-        required: true
+        default: false
+    },
+    resetToken: {
+        type: String,
     }
 });
 
-module.exports = model('User', UserSchema)
+UserSchema.pre('save', async function save(next) {
+    if (!this.isModified('password')) return next();
+    try {
+        const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+        this.password = await bcrypt.hash(this.password, salt);
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+});
+
+UserSchema.methods.validatePassword = async function validatePassword(data) {
+    return bcrypt.compare(data, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema)
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,6 +4,26 @@ const router = express.Router();
 
 router.get("/",(req, res)=>{
     res.send("We are the Users!");
-});
+}); 
+
+router.post("/signin",async (req,res)=>{
+    const mongoose = require('mongoose')
+    const User = require('../models/user.model')
+    const vUser=await User.findOne({username: req.body.username}).exec()
+    console.log(vUser);
+    var bool = await vUser.validatePassword(req.body.password)
+    console.log(bool);
+    if(await vUser.validatePassword(req.body.password)) {
+        res.json({message: "Erfolgreicher SignIn von " + req.body.username});
+    } else {
+        res.json({message: "Fehler! Login von " + req.body.username +" nicht möglich!"});
+    }
+    
+})
+
+router.post("/signup",(req,res)=>{
+    res.json({message: "Erfolgreicher Signup von "+req.body.username})
+})
+
 
 module.exports = router;
