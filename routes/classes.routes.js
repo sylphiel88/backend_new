539f494449878086ed2781ce458ac42015e9d217
sylphiel88 <jihmels@gmail.com>
2021-11-12 12:01:57 +0100

    departmentCreator

--- routes/classes.routes.js
+++ routes/classes.routes.js
@@ -6,7 +6,6 @@ const Classes = require('../models/classes.model')
 const Dozent = require('../models/dozent.model');
 const User = require('../models/user.model')
 const moment = require('moment');
-const { deepmerge } = require("@mui/utils");
 
 router.get('/', (req, res) => {
     res.send("we are the classes")
@@ -36,25 +35,36 @@ router.post('/department', async (req, res) => {
     }
 })
 
-router.post('/departmentDel', async(req,res)=>{
-    var dep = req.body.dep
-    var doc = req.body.doc
-    var docId = await User.findOne({username:doc},"_id")
-    docId = docId._id
-    var depId = await Department.findOne({nameLong:dep},"_id")
-    depId = depId._id
-    await Dozent.updateOne({userId:docId}, {$pull: {departments:depId}})
+router.post('/departmentD', async (req, res) => {
+    const ds = req.body.ds;
+    var dId = Department.findOne({nameShort:ds},"_id")
+    dId = dId._id
+    Classes.deleteMany({departments:dId})
+    Dozent.updateMany({$in:{departments:dId}},{$pull: {departments:dId}})
+    await Department.deleteOne({nameShort:ds})
     res.end()
 })
 
-router.post('/departmentUpd', async(req, res)=>{
+
+router.post('/departmentDel', async (req, res) => {
+    var dep = req.body.dep
+    var doc = req.body.doc
+    var docId = await User.findOne({ username: doc }, "_id")
+    docId = docId._id
+    var depId = await Department.findOne({ nameLong: dep }, "_id")
+    depId = depId._id
+    await Dozent.updateOne({ userId: docId }, { $pull: { departments: depId } })
+    res.end()
+})
+
+router.post('/departmentUpd', async (req, res) => {
     const dep = req.body.dep
     const doc = req.body.doc
-    var docId = await User.findOne({username:doc},"_id")
+    var docId = await User.findOne({ username: doc }, "_id")
     docId = docId._id
-    var depId = await Department.findOne({nameLong:dep},"_id")
+    var depId = await Department.findOne({ nameLong: dep }, "_id")
     depId = depId._id
-    await Dozent.updateOne({userId:docId}, {$push: {departments:depId}})
+    await Dozent.updateOne({ userId: docId }, { $push: { departments: depId } })
     res.end()
 })
 
@@ -62,17 +72,36 @@ router.post('/class', async (req, res) => {
     const cs = req.body.cs;
     const ds = req.body.ds;
     const ns = req.body.ns;
-    const city = await City.findOne({ nameShort: cs }).exec()
-    const department = await Department.findOne({ nameShort: ds }).exec()
-    const newCl = await new Classes({ year: Date.now(), num: ns });
-    newCl.city = city
-    newCl.department = department
-    try {
-        await newCl.save()
-            .then(() => { res.json({ msg: newCl.nameLong }) });
-    } catch (e) {
-        res.json({ error: e })
-    }
+    const y = parseInt(req.body.y)+2000;
+    var city = await City.findOne({ nameShort: cs },"_id")
+    city = city._id
+    var department = await Department.findOne({ nameShort: ds },"_id")
+    department = department._id
+    const newCl = await new Classes({ year: new Date(y.toString() + "-01-01T00:00:00.000+00:00"), num: ns, city:city, department:department });
+    newCl.save()
+    num = parseInt(ns)+1
+    res.json({num: num})
+})
+
+router.post('/classD', async (req, res) => {
+    const cs = req.body.cs;
+    const ds = req.body.ds;
+    const ns = req.body.ns;
+    const y = parseInt(req.body.y)+2000;
+    const city = await City.findOne({ nameShort: cs })
+    const department = await Department.findOne({ nameShort: ds })
+    const firstDay = new Date(y.toString() + "-01-01T00:00:00.000+00:00")
+    const lastDay = new Date(y.toString() + "-12-31T23:59:59.999+00:00")
+    await Classes.deleteOne({
+        city: { $in: city },
+        department: { $in: department },
+        year: {
+            $gte: firstDay,
+            $lt: lastDay
+        },
+        num:{$in: ns}
+    })
+    res.json({num: ns})
 })
 
 router.get('/getAllClasses', async (req, res) => {
@@ -82,9 +111,25 @@ router.get('/getAllClasses', async (req, res) => {
 
 router.get('/getAllDepartments', async (req, res) => {
     var depArr = []
-    const deps = await Department.find({}, 'nameLong').exec()
+    const deps = await Department.find({}, 'nameLong')
     deps.map(x => { depArr.push(x.nameLong) })
     res.json({ deps: depArr })
+})
+
+router.get('/getAllDepartmentsS', async (req, res) => {
+    var depArr = []
+    const deps = await Department.find({}, 'nameShort')
+    console.log(deps)
+    deps.map(x => { depArr.push(x.nameShort) })
+    console.log(depArr);
+    res.json({ deps: depArr })
+})
+
+router.get('/getAllCitysS', async (req, res) => {
+    var cityArr = []
+    const citys = await City.find({}, 'nameShort').exec()
+    citys.map(x => { cityArr.push(x.nameShort) })
+    res.json({ citys: cityArr })
 })
 
 router.get('/getDocents', async (req, res) => {
@@ -113,7 +158,45 @@ router.post('/getDepsClasses', async (req, res) => {
         clsNL.push(x.department.nameShort + "-" + x.city.nameShort + "-" + year + "-" + num)
         clsIds.push(x._id)
     })
-    res.json({ classes: clsNL, ids: clsIds})
+    res.json({ classes: clsNL, ids: clsIds })
+})
+
+router.post('/getAllNums', async (req, res) => {
+    var numArr=[]
+    var dep = req.body.dep
+    var town = req.body.town
+    var year = parseInt(req.body.year)+2000
+    var depId = await Department.findOne({ "nameShort": dep },'_id')
+    depId = depId._id
+    var tId = await City.findOne({ "nameShort": town })
+    tId = tId._id
+    const firstDay = new Date(year.toString() + "-01-01T00:00:00.000+00:00")
+    const lastDay = new Date(year.toString() + "-12-31T23:59:59.999+00:00")
+    var num = await Classes.find({
+        city: { $in: tId },
+            department: { $in: depId },
+            year: {
+                $gte: firstDay,
+                $lt: lastDay
+            }
+    },"num")
+    num.forEach(n => numArr.push(n.num))
+    res.json({nums:numArr})
+})
+
+router.get('/getAllClassesNL', async (req, res) => {
+    var clsNL = []
+    var clsIds = []
+    const classes = await Classes.find({}).populate('city').populate('department');
+    classes.forEach(x => {
+        console.log(x);
+        const year = moment(x.year).format('YY')
+        var num = x.num.toString()
+        num = num.length == 1 ? "0" + num : num
+        clsNL.push(x.department!=null?(x.department.nameShort + "-" + x.city.nameShort + "-" + year + "-" + num):null)
+        clsIds.push(x.department!=null?x._id:null)
+    })
+    res.json({ classes: clsNL, ids: clsIds })
 })
 
 router.post('/getDocentsDepartments', async (req, res) => {
@@ -151,7 +234,7 @@ router.post('/getClassesOfDocent', async (req, res) => {
         classes.forEach(klasse => {
             var year = moment(klasse.year).format("YY")
             var num = klasse.num.toString().length == 1 ? "0" + klasse.num.toString() : klasse.num.toString()
-            var nameLong = klasse.department.nameShort + "-" + klasse.city.nameShort + "-" + year + "-" + num
+            var nameLong = klasse.department!=null?klasse.department.nameShort + "-" + klasse.city.nameShort + "-" + year + "-" + num:null
             classArr.push(nameLong)
             classIds.push(klasse._id)
         });
@@ -159,22 +242,46 @@ router.post('/getClassesOfDocent', async (req, res) => {
     res.json({ classN: classArr, classIds: classIds })
 })
 
-router.post('/classDel', async(req,res)=>{
+router.post('/classDel', async (req, res) => {
     var clsId = req.body.cls
     var doc = req.body.doc
-    var docId = await User.findOne({username:doc},"_id")
+    var docId = await User.findOne({ username: doc }, "_id")
     docId = docId._id
-    await Dozent.updateOne({userId:docId}, {$pull: {classes:clsId}})
+    await Dozent.updateOne({ userId: docId }, { $pull: { classes: clsId } })
     res.end()
 })
 
-router.post('/classUpd', async(req,res)=>{
+router.post('/classUpd', async (req, res) => {
     var clsId = req.body.cls
     var doc = req.body.doc
-    var docId = await User.findOne({username:doc},"_id")
+    var docId = await User.findOne({ username: doc }, "_id")
     docId = docId._id
-    await Dozent.updateOne({userId:docId}, {$push: {classes:clsId}})
+    await Dozent.updateOne({ userId: docId }, { $push: { classes: clsId } })
     res.end()
+})
+
+router.post('/getNum', async (req, res) => {
+    const town = req.body.town
+    const dep = req.body.dep
+    var year = req.body.year
+    year = parseInt(year) + 2000
+    var townId = await City.findOne({ 'nameShort': town }, "_id")
+    townId = townId._id
+    var depId = await Department.findOne({ 'nameShort': dep }, "_id")
+    depId = depId._id
+    const firstDay = new Date(year.toString() + "-01-01T00:00:00.000+00:00")
+    const lastDay = new Date(year.toString() + "-12-31T23:59:59.999+00:00")
+    var num =
+        await Classes.find({
+            city: { $in: townId },
+            department: { $in: depId },
+            year: {
+                $gte: firstDay,
+                $lt: lastDay
+            }
+        }).sort({ 'num': -1 }).limit(1)
+    const numb = num.length > 0 ? num[0].num + 1 : 1
+    res.json({ num: numb })
 })
 
 
